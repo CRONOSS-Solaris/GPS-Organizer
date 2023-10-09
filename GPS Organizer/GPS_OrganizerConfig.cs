4a76a625974b02d2243255826d005d960e25525c
CRONOSS <122462572+CRONOSS-Solaris@users.noreply.github.com>
2023-10-09 08:35:40 +0200

    removal of unnecessary code

--- GPS Organizer/GPS_OrganizerConfig.cs
+++ GPS Organizer/GPS_OrganizerConfig.cs
@@ -10,19 +10,4 @@ public class GPS_OrganizerConfig : ViewModel
     public bool SendMarkerOnJoin { get => _sendMarkerOnJoin; set => SetValue(ref _sendMarkerOnJoin, value); }
     public bool DebugMode { get => _debugMode; set => SetValue(ref _debugMode, value); }
 
-    public void Save(string filePath)
-    {
-        using (var writer = new XmlTextWriter(filePath, System.Text.Encoding.UTF8))
-        {
-            writer.Formatting = Formatting.Indented;
-            writer.WriteStartDocument();
-
-            writer.WriteComment("DebugMode: When set to true, additional logging information will be output to assist with debugging. Useful for tracking the behavior and state of the plugin in detail.");
-
-            var serializer = new XmlSerializer(typeof(GPS_OrganizerConfig));
-            serializer.Serialize(writer, this);
-
-            writer.WriteEndDocument();
-        }
-    }
 }
